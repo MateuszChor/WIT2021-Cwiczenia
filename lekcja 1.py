5f6f31d680ab3895f28b3634c7017ed44058bc42
mat bombka <bitpit11@wp.pl>
2021-10-10 18:02:35 +0200

    Add new files

---
+++ lekcja 1.py
@@ -0,0 +1,10 @@
+
+rating=input(" Oceń swój dzień w skali od 0 do 10 :")
+print(f"Oceniłeś swój dzień w skali od 0 do 10 na {rating}")
+
+name=input("Jak masz na imie ? :")
+age=int(input("ile masz lat ? :"))
+
+print(f"Twoje imie to {name} i masz {age} lat")
+
+
