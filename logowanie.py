2bcdfa2b27f066061aff2ed0f9a327e1a7ef88ad
MateuszChor <79656363+MateuszChor@users.noreply.github.com>
2021-11-06 12:59:37 +0100

    Delete logowanie.py

--- logowanie.py
+++
@@ -1,7 +0,0 @@
-user_is_authenticated = False
-while not user_is_authenticated:
-    login = input("Podaj login: ")
-    password = input("Podaj haslo: ")
-    if login == 'Admin' and password == '1234':
-        user_is_authenticated = True
-print("Authenticated")
