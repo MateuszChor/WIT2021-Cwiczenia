adb2dccc38dbfe3ddd7a091b4fab22403f0b63be
MateuszChor <79656363+MateuszChor@users.noreply.github.com>
2022-01-03 00:30:10 +0100

    UnitTest

---
+++ 6 weekend/Zad2.py
@@ -0,0 +1,23 @@
+class Employee:
+    def __init__(self, first_name, race,age,salary):
+        self.first_name = first_name
+        self.last_name = race
+        self.age = age
+        self.salary = salary
+
+    def introduce_self(self):
+        return f'My name is {self.first_name} {self.last_name} and I am {self.age} years old'
+
+    def raise_salary(self, ratio):
+        self.salary = self.salary * ratio
+        return self.salary
+
+    def check_age(self):
+        if self.age < 18:
+            return 'Underage employee'
+        else:
+            return 'Adult employee'
+
+    def get_email(self):
+        return f'{self.first_name}{self.last_name}@company.com'
+
