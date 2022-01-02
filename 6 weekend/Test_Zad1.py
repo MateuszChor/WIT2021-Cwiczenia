3e103a8d9c9739c35452950b3cfeaf2d343dca9e
MateuszChor <79656363+MateuszChor@users.noreply.github.com>
2022-01-02 23:32:43 +0100

    zad1

--- 6 weekend/Test_Zad1.py
+++ 6 weekend/Test_Zad1.py
@@ -1,1 +1,22 @@
 import  unittest
+import  Zad1_skrypt as Skrypt
+
+class TestEmployee(unittest.TestCase):
+
+    def test_add(self):
+        result_add = Skrypt.add(10, 5)
+        self.assertEqual(result_add, 15)
+    def test_subtract(self):
+        result_subtract = Skrypt.subtract(10, 5)
+        self.assertEqual(result_subtract,5)
+    def test_multipy(self):
+        result_multiply = Skrypt.multiply(10, 5)
+        self.assertEqual(result_multiply,50)
+    def test_divide(self):
+        result_divide = Skrypt.divide(10, 5)
+        self.assertEqual(result_divide, 2)
+        self.assertRaises(ValueError,Skrypt.divide(10,0))
+
+
+if __name__ == '__main__':
+    unittest.main()
